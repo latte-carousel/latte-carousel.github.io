a308e6bf08a1e08ae7bb41c53ecc7be0880fcbec
Felipe Garcia <felipefegar@gmail.com>
2018-06-02 16:55:54 +0000

    Refactored page

---
+++ js/demo.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', function () {
+    var root = document.querySelector('.latte-carousel');
+
+    var options = {
+        count: 3,
+        touch: true,
+        buttons: true,
+        dots: true,
+        rewind: true,
+        autoplay: 0,
+        responsive: {
+            "0": {
+                count: 1.5,
+                touch: true,
+                buttons: false
+            },
+            "480": {
+                count: 2.5,
+                touch: true,
+                buttons: false
+            },
+            "768": {
+                count: 3,
+                touch: false,
+                dots: false
+            },
+            "1440": {
+                count: 4,
+                touch: false,
+                dots: false
+            }
+        }
+    };
+
+    var carousel = new latte.Carousel(root, options);
+});
