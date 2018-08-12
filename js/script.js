c83d0dbb62827132424c01fb2f672a35e63ca19b
Felipe Garcia <felipefegar@gmail.com>
2018-08-12 10:58:11 +0000

    Updated version

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 hljs.initHighlightingOnLoad();
+
+window.addEventListener("DOMContentLoaded", function() {
+    var btnGetStarted = document.getElementById("btn-get-started");
+    var btnNpm = document.getElementById("btn-npm");
+    var btnGithub = document.getElementById("btn-github");
+
+    btnGetStarted.addEventListener("mouseup", function() {
+        gtag("event", "get-started");
+    });
+
+    btnNpm.addEventListener("mouseup", function() {
+        gtag("event", "npm");
+    });
+
+    btnGithub.addEventListener("mouseup", function() {
+        gtag("event", "github");
+    });
+});
