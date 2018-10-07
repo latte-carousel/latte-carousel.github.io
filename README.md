c0248dae75d8e239884e93d8328dae4c04326451
Felipe Garcia <felipefegar@gmail.com>
2018-10-07 12:52:09 +0000

    Site v2

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 LatteCarousel is a lightweight and responsive carousel without any dependencies.
 
 [Download](https://github.com/latte-carousel/latte-carousel)
+
+Docs and logo licensed **CC-BY-4.0**, code examples **MIT**.
---
+++ sw.js
@@ -0,0 +1,99 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v3.6.2/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.6.2"});
+
+workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "webpack-runtime-165e131cc88a57645efe.js"
+  },
+  {
+    "url": "app.c874a54884440657145d.css"
+  },
+  {
+    "url": "app-c182676350a370c6f45d.js"
+  },
+  {
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-1cc5146d47353001288c.js"
+  },
+  {
+    "url": "index.html",
+    "revision": "73d846f4875b996a8c50f812dd9fc470"
+  },
+  {
+    "url": "offline-plugin-app-shell-fallback/index.html",
+    "revision": "dd172b01a43d8cdef95194ec812615ce"
+  },
+  {
+    "url": "component---src-pages-index-js.de7c7b2874420632b7ef.css"
+  },
+  {
+    "url": "4-8aae338b2970e2ac8add.js"
+  },
+  {
+    "url": "component---src-pages-index-js-74b1515e4f8f457ba0a3.js"
+  },
+  {
+    "url": "static/d/173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E.json",
+    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
+  },
+  {
+    "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
+    "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
+  },
+  {
+    "url": "manifest.webmanifest",
+    "revision": "9d4844f6dc6dc5a0f7762d8c9223541b"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/offline-plugin-app-shell-fallback/index.html", {
+  whitelist: [/^[^?]*([^.?]{5}|\.html)(\?.*)?$/],
+  blacklist: [/\?(.+&)?no-cache=1$/],
+});
+
+workbox.routing.registerRoute(/\.(?:png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https:/, workbox.strategies.networkFirst(), 'GET');
+"use strict";
+
+/* global workbox */
+self.addEventListener("message", function (event) {
+  var api = event.data.api;
+
+  if (api === "gatsby-runtime-cache") {
+    var resources = event.data.resources;
+    var cacheName = workbox.core.cacheNames.runtime;
+    event.waitUntil(caches.open(cacheName).then(function (cache) {
+      return Promise.all(resources.map(function (resource) {
+        return cache.add(resource).catch(function (e) {
+          // ignore TypeErrors - these are usually due to
+          // external resources which don't allow CORS
+          if (!(e instanceof TypeError)) throw e;
+        });
+      }));
+    }));
+  }
+});
